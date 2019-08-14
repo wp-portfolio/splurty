806048bf4824943346aaab131b96ea81e51fb332
wp-portfolio <wpanowyk1@gmail.com>
2019-08-14 20:39:35 +0000

    fixed flash error to be html safe

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,4 @@
 class Quote < ApplicationRecord
-#code goes here
+  validates :saying, presence: true, length: { maximum: 140, minimum: 3 }
+  validates :author, presence: true, length: { maximum: 50, minimum: 3}
 end
